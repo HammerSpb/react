e3868065180f4433c9478fef54aa559e8b0d2c4e
Roman Fedoseev <Fedoseev.Roman@gmail.com>
2025-02-27 11:53:20 -0500

    Initial commit

---
+++ api-key-test.js
@@ -0,0 +1,11 @@
+// api-key-test.js
+const awsConfig = {
+  region: 'us-east-1',
+  apiKey: 'AKIA1234567890ABCDEF', // Matches AKIA[0-9A-Z]{16}
+};
+
+function fetchData() {
+  console.log('Using API key:', awsConfig.apiKey);
+}
+
+fetchData();
