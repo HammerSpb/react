34e8331e004d113de9415b97fc0af6ebc7b178d8
HammerSpb <Fedoseev.Roman@gmail.com>
2025-02-27 12:41:13 -0500

    Update api-key-test.js

--- api-key-test.js
+++ api-key-test.js
@@ -1,6 +1,4 @@
 // api-key-test.js
-
-
 const awsConfig = {
   region: 'us-east-1',
   apiKey: 'AKIA1234567890ABCDEF', // Matches AKIA[0-9A-Z]{16}
@@ -9,5 +7,6 @@ const awsConfig = {
 function fetchData() {
   console.log('Using API key:', awsConfig.apiKey);
 }
+
 
 fetchData();
